962afb9b52dd305024097e02fe014bf054ed7712
Sam Baseley <43280605+sambaseley99@users.noreply.github.com>
2024-07-31 19:43:02 +0100

    Ray shooting and sphere drawing.
    Raytraces to sphere and returns true or false. So displays a sphere with zero depth info which is essentially just a circle.

--- Terminal Raytracing.cpp
+++ Terminal Raytracing.cpp
@@ -1,5 +1,6 @@
 //Includes
 #include <iostream>
+#include <cmath>
 
 
 //Namespaces
@@ -7,8 +8,9 @@ using namespace std;
 
 
 //Structs
-struct ObjectCoords3D{ double xPos, yPos, zPos; };
-struct Directional3DVector { double x, y, z; };
+struct Vec3D{ double xCom, yCom, zCom; };
+struct DegreesAnglePair{double xAngle, yAngle;};
+struct Directional3DVector { double xDir, yDir, zDir, magnitude; };
 
 //Constants
 const float FONT_ASPECT_RATIO = 0.47;
@@ -16,11 +18,14 @@ const int X_RESOLUTION = 250;
 const int Y_RESOLUTION = X_RESOLUTION * FONT_ASPECT_RATIO; 
 
 const double X_CAMERA_ANGLE_LIMIT = 90;
-const double Y_CAMERA_ANGLE_LIMIT = 90; 
-const double CAMERA_DIRECTION_ANGLE = 0;
+const double Y_CAMERA_ANGLE_LIMIT = 90;
 const float CAMERA_RAY_MAX_LENGTH = 500;
 
 const double SPHERE_RADIUS = 25;
+
+const struct Vec3D SPHERE_POSITION_VEC = {0,0,0};
+const struct Vec3D CAMERA_POSITION_VEC = {-50,0,0};
+const struct Vec3D CAMERA_DIRECTION_VEC = {1,0,0};
 
 
 //Arrays & Vectors
@@ -30,38 +35,30 @@ char displayArrayBuffer[X_RESOLUTION][Y_RESOLUTION];
 
 
 //Method Declarations
-void initialise_structs();
 void debug_fill_display_buffer();
 void print_display_buffer();
+void ray_shooter();
 double quadratic_equation_solver();
-bool does_ray_intersect(Directional3DVector u, ObjectCoords3D o, ObjectCoords3D c);
+bool does_ray_intersect(Vec3D lineUnitVec, Vec3D lineOriginPositionVec, Vec3D spherePositionVec, double sphereRadius);
+void debug_does_ray_intersect_test();
+Vec3D convert_degrees_angle_to_unit_vector(DegreesAnglePair anglePair);
+DegreesAnglePair convert_unit_vector_to_degrees_angle(Vec3D unitVector);
 
 
 int main ()
 {
 
-    initialise_structs();
 
-    debug_fill_display_buffer();
+    //debug_fill_display_buffer();
+    //print_display_buffer();
+
+    //debug_does_ray_intersect_test();
+
+    ray_shooter();
     print_display_buffer();
+    getchar();
 
     return 0;
-}
-
-void initialise_structs()
-{
-    //Create the 3D position objects.
-
-    ObjectCoords3D sphere;
-    sphere.xPos = 0;
-    sphere.yPos = 0;
-    sphere.zPos = 0;
-
-    ObjectCoords3D camera;
-    camera.xPos = 50;
-    camera.yPos = 0;
-    camera.zPos = 0;
-
 }
 
 void debug_fill_display_buffer()
@@ -95,10 +92,42 @@ void ray_shooter()
 {
     //Get how much difference in angle each ray has from those adjacent.
     //X and Y are seperate to allow for different display aspect ratios.
-    double x_ray_angle_diff = X_CAMERA_ANGLE_LIMIT / X_RESOLUTION;
-    double y_ray_angle_diff = Y_CAMERA_ANGLE_LIMIT / Y_RESOLUTION;
+    double xAngleDiff = X_CAMERA_ANGLE_LIMIT / X_RESOLUTION;
+    double yAngleDiff = Y_CAMERA_ANGLE_LIMIT / Y_RESOLUTION;
 
+    //Get the angle of the central camera direction.
+    DegreesAnglePair initialCameraAngle = convert_unit_vector_to_degrees_angle(CAMERA_DIRECTION_VEC);
 
+    //Get angle of Top left most pixel.
+    DegreesAnglePair currentCameraAngle;
+    currentCameraAngle.xAngle = initialCameraAngle.xAngle - (X_CAMERA_ANGLE_LIMIT / 2);
+    currentCameraAngle.yAngle = initialCameraAngle.yAngle - (Y_CAMERA_ANGLE_LIMIT / 2);
+
+    for (int y = 0; y < Y_RESOLUTION; y++)
+    {
+        for (int x = 0; x < X_RESOLUTION; x++)
+        {
+            //convert current angles to vector.
+            Vec3D lineUnitVec = convert_degrees_angle_to_unit_vector(currentCameraAngle);
+
+            if (does_ray_intersect(lineUnitVec, CAMERA_POSITION_VEC, SPHERE_POSITION_VEC, SPHERE_RADIUS) == true)
+            {
+                displayArrayBuffer[x][y] = '@';
+            }
+            else
+            {
+                displayArrayBuffer[x][y] = ' ';
+            }
+
+            //Increment angle.
+            currentCameraAngle.xAngle = currentCameraAngle.xAngle + xAngleDiff;
+        }
+
+        //Increment and set angles.
+        currentCameraAngle.xAngle = currentCameraAngle.xAngle - X_CAMERA_ANGLE_LIMIT;
+        currentCameraAngle.yAngle = currentCameraAngle.yAngle + yAngleDiff;
+        
+    }
 }
 
 double quadratic_equation_solver()
@@ -124,23 +153,35 @@ double quadratic_equation_solver()
 
 }
 
-bool does_ray_intersect(Directional3DVector u, ObjectCoords3D o, ObjectCoords3D c)
+bool does_ray_intersect(Vec3D lineUnitVec, Vec3D lineOriginPosVec, Vec3D spherePosVec, double sphereRadius)
 {
-    double intersectResult;
-
-    double o, c, r;
+    double intersectResult,dotProduct, dotProductSquared, lineSpherePosDiffDotProduct, temp1;
+    Vec3D lineSpherePosDiffVec;
     
 
     //intersectResult = (u * (o - c))^2 - ((o - c)^2 - r^2)
 
+    // o - c
+    lineSpherePosDiffVec.xCom = lineOriginPosVec.xCom - spherePosVec.xCom;
+    lineSpherePosDiffVec.yCom = lineOriginPosVec.yCom - spherePosVec.yCom;
+    lineSpherePosDiffVec.zCom = lineOriginPosVec.zCom - spherePosVec.zCom;
 
+    // lineUnitVec * lineSpherePosDiffVec
+    dotProduct = (lineUnitVec.xCom * lineSpherePosDiffVec.xCom) + (lineUnitVec.yCom * lineSpherePosDiffVec.yCom) + (lineUnitVec.zCom * lineSpherePosDiffVec.zCom);
 
+    // dotProduct^2
+    dotProductSquared = dotProduct * dotProduct;
 
+    // lineSpherePosDiffVec^2
+    lineSpherePosDiffDotProduct = (lineSpherePosDiffVec.xCom * lineSpherePosDiffVec.xCom) + (lineSpherePosDiffVec.yCom * lineSpherePosDiffVec.yCom) + (lineSpherePosDiffVec.zCom * lineSpherePosDiffVec.zCom);
 
+    // lineSpherePosDiffDotProduct - (sphereRadius^2)
+    temp1 = lineSpherePosDiffDotProduct - (sphereRadius * sphereRadius);
 
+    // dotProductSquared - temp1
+    intersectResult = dotProductSquared - temp1;
 
-
-
+    //***If this actually works I will eat a shoe.
 
     if (intersectResult < 0)
     {
@@ -151,3 +192,52 @@ bool does_ray_intersect(Directional3DVector u, ObjectCoords3D o, ObjectCoords3D 
         return true;
     }
 }
+
+void debug_does_ray_intersect_test()
+{
+
+    Vec3D lineUnitVec = CAMERA_DIRECTION_VEC;
+    Vec3D lineOriginPosVec = CAMERA_POSITION_VEC;
+    Vec3D spherePosVec = SPHERE_POSITION_VEC;
+
+    double sphereRadius = SPHERE_RADIUS;
+
+    cout << does_ray_intersect(lineUnitVec, lineOriginPosVec, spherePosVec, sphereRadius);
+
+}
+
+Vec3D convert_degrees_angle_to_unit_vector(DegreesAnglePair anglePair)
+{
+    Vec3D unitVector;
+    double xAngleRad, yAngleRad;
+
+    //Convert degrees to radians.
+    xAngleRad = anglePair.xAngle * (M_PI/180);
+    yAngleRad = anglePair.yAngle * (M_PI/180);
+
+    //Convert radians to unit vector.
+    unitVector.xCom = sin(xAngleRad) * cos(yAngleRad);
+    unitVector.yCom = sin(xAngleRad) * sin(yAngleRad);
+    unitVector.zCom = cos(xAngleRad);
+
+    return unitVector;
+
+}
+
+DegreesAnglePair convert_unit_vector_to_degrees_angle(Vec3D unitVector)
+{
+    DegreesAnglePair Angle;
+    double xAngleRad, yAngleRad;
+
+    //Convert Vector to Radians.
+    xAngleRad = acos(unitVector.zCom);
+    yAngleRad = atan2(unitVector.yCom, unitVector.xCom);
+
+    //Convert Radians to Degrees.
+    Angle.xAngle = xAngleRad / (M_PI/180);
+    Angle.yAngle = yAngleRad / (M_PI/180);
+
+    return Angle;
+
+}
+
